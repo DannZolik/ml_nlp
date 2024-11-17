08bb3e9c3b1f411d64630119b89f02b9416fe7af
DannZolik <danjuha1@gmail.com>
2024-11-17 15:19:03 +0200

    fix

--- main6.py
+++ main6.py
@@ -1,3 +1,6 @@
+from deep_translator import GoogleTranslator
+from lingua import LanguageDetectorBuilder
+
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import stopwords
 from nltk.probability import FreqDist
@@ -7,9 +10,13 @@ import nltk
 nltk.download('stopwords')
 nltk.download('punkt_tab')
 
+detector = LanguageDetectorBuilder.from_all_languages().with_preloaded_language_models().build()
+
 def summarize_text(article):
-    sentences = sent_tokenize(article)
-    words = word_tokenize(article.lower())
+    input_language = detector.detect_language_of(article).iso_code_639_1.name.lower()
+    input_translation = GoogleTranslator(source=input_language, target='en').translate(article)
+    sentences = sent_tokenize(input_translation)
+    words = word_tokenize(input_translation.lower())
     words = [word for word in words if word.isalnum() and word not in stopwords.words()]
 
     freq_dist = FreqDist(words)
@@ -26,12 +33,13 @@ def summarize_text(article):
     ranked_sentences = sorted(sentence_scores, key=sentence_scores.get, reverse=True)
 
     summary = " ".join(ranked_sentences[:2])
-    return summary
+
+    output_tranlate = GoogleTranslator(source='en', target=input_language).translate(summary)
+
+    return output_tranlate
 
 article = (
-    "Latvija ir valsts Baltijas reģionā. Tās galvaspilsēta ir Rīga, kas ir slavena ar savu "
-    "vēsturisko centru un skaistajām ēkām. Latvija robežojas ar Lietuvu, Igauniju un Krieviju, "
-    "kā arī tai ir piekļuve Baltijas jūrai. Tā ir viena no Eiropas Savienības dalībvalstīm."
+    "Latvija ir valsts Baltijas reģionā. Tās galvaspilsēta ir Rīga, kas ir slavena ar savu vēsturisko centru un skaistajām ēkām. Latvija robežojas ar Lietuvu, Igauniju un Krieviju, kā arī tai ir piekļuve Baltijas jūrai. Tā ir viena no Eiropas Savienības dalībvalstīm."
 )
 
 summary = summarize_text(article)
